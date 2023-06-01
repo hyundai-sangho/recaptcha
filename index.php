d794f1fef88837681508bf1fd416063f3a070ac0
조상호 <hyundai_sangho@naver.com>
2023-06-01 11:23:10 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+
+/* php dotenv 사용을 위해 vendor 폴더 내부의 autoload.php require 함. */
+require_once "vendor/autoload.php";
+
+/* php dotenv 사용법 */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// 구글 리캡챠 사이트 키값을 입력한다.
+const SITEKEY = $_ENV['SITEKEY'];
+
+?>
+
+<!-- 스타일 -->
+<style>
+  .form {
+    margin: 30px auto;
+    border: solid 1px #eee;
+    max-width: 400px;
+    width: 100%;
+    padding: 1%;
+  }
+
+  .form .submit {
+    margin: 20px 0;
+  }
+</style>
+
+<div class="form">
+  <form id="server" action="server.php" method="post">
+    <input type="hidden" name="recatchaResponse" value="">
+    <div class="recaptcha">
+      <div class="g-recaptcha" data-sitekey="<?php echo SITEKEY ?>"></div>
+    </div>
+    <div class="submit">
+      <input type="submit" value="등록">
+    </div>
+  </form>
+
+  <!-- jquery 3.5.1 -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+  <!-- 리캽챠 API -->
+  <script src="https://www.google.com/recaptcha/api.js"></script>
+
+
+  <!-- 스크립트 -->
+  <script>
+    var authSubmit = true;
+
+    $(document).on('submit', '#server', function () {
+      if (authSubmit !== true) {
+        alert("잠시만 기다려 주세요.");
+
+        return false;
+      }
+
+      var recatchaResponse = grecaptcha.getResponse()
+
+      if (!recatchaResponse) {
+        alert("로봇이 아닙니다에 체크해주세요.");
+
+        return false;
+      }
+
+      var $server = $('#server');
+      $server.find('[name="recatchaResponse"]').val(recatchaResponse);
+      authSubmit = false;
+
+      // ajax 통신
+      $.ajax(
+        {
+          url: $server.attr('action'),
+          type: $server.attr('method'),
+          dataType: 'json',
+          data: $server.serialize()
+        }
+      )
+        .done(function (e) {
+          alert(e.msg);
+
+          return false;
+        })
+        .fail(function (e) {
+          alert(e.responseText);
+        })
+        .always(function () {
+          authSubmit = true;
+          grecaptcha.reset(); // 리캡챠 초기화
+
+          $server.find('[name="recatchaResponse"]').val('');
+        })
+
+      return false;
+    });
+  </script>
+</div>
---
+++ server.php
@@ -0,0 +1,102 @@
+<?php
+
+/* php dotenv 사용을 위해 vendor 폴더 내부의 autoload.php require 함. */
+require_once "vendor/autoload.php";
+
+/* php dotenv 사용법 */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// 리캽챠 서버단 프로그램 예제
+
+// 구글 리캡챠 비밀키 값을 입력한다.
+const SECRET_KEY = $_ENV['SECRET_KEY'];
+
+extract($_POST);
+
+if (empty($recatchaResponse)) {
+
+  die(
+    json_encode(
+      array(
+        'rst' => 'fail',
+        'msg' => '`로봇이 아닙니다.`에 체크해주세요.'
+      )
+    )
+  );
+}
+
+// API URL 데이터
+$apiData = array(
+  "secret" => SECRET_KEY,
+  "response" => $recatchaResponse,
+  "remoteip" => $_SERVER['REMOTE_ADDR'],
+);
+
+// API URL 셋팅
+$url = "https://www.google.com/recaptcha/api/siteverify?" . http_build_query($apiData);
+
+// CURL 통신
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_REFERER, 'http' . (!empty($_SERVER['HTTPS']) ? 's' : null) . '://' . $_SERVER['HTTP_HOST']);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 0);
+
+$response = curl_exec($ch);
+
+if (curl_error($ch)) {
+  $response = array(
+    'curl_error' => true,
+    'msg' => 'CURL Error(' . curl_errno($ch) . ') ' . curl_error($ch)
+  );
+}
+curl_close($ch);
+
+// 통신 에러 처리
+if (isset($response['curl_error'])) {
+  die(
+    json_encode(
+      array(
+        'rst' => 'fail',
+        'msg' => $response['msg']
+      )
+    )
+  );
+}
+
+if (empty($response)) {
+  die(
+    json_encode(
+      array(
+        'rst' => 'fail',
+        'msg' => '응답에러'
+      )
+    )
+  );
+}
+
+$responseData = json_decode($response, true);
+
+if (empty($responseData['success']) || $responseData['success'] !== true) {
+  die(
+    json_encode(
+      array(
+        'rst' => 'fail',
+        'msg' => '인증 실패'
+      )
+    )
+  );
+} else {
+  die(
+    json_encode(
+      array(
+        'rst' => 'success',
+        'msg' => '인증 성공!'
+      )
+    )
+  );
+}
